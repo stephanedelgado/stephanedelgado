e33194bd2f0595b0fa9f3b09e536fd72ee8e3b47
Stephane Delgado <stephanedelgado@gmail.com>
2017-03-13 14:54:42 +0800

    rails new

--- rails-longest-word-game/config/routes.rb
+++ rails-longest-word-game/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-
-  get 'pages/contact'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
